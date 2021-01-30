45ae1a26bef4833aa1893a745db3c425486b086c
Ying Yang <yingyang9416@gmail.com>
2021-01-30 17:04:37 -0500

    added tab bar view controller

--- KidsFit/KidsFit/AppDelegate.swift
+++ KidsFit/KidsFit/AppDelegate.swift
@@ -10,10 +10,11 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
         return true
     }
 
