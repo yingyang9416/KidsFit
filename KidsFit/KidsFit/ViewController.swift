bc40921a64e764b349a2b980a3efaf953cc25cc2
Ying Yang <yingyang9416@gmail.com>
2021-01-30 14:29:19 -0500

    initialize project

---
+++ KidsFit/KidsFit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KidsFit
+//
+//  Created by Steven Yang on 1/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
