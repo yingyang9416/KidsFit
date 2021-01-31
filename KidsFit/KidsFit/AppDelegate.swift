032d35024909ade0d7dbb91165bcf08eb4eb274c
Ying Yang <yingyang9416@gmail.com>
2021-01-30 19:12:30 -0500

    init pod directory and Firebase project

--- KidsFit/KidsFit/AppDelegate.swift
+++ KidsFit/KidsFit/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 //        window = UIWindow(frame: UIScreen.main.bounds)
 //        window?.makeKeyAndVisible()
+        FirebaseApp.configure()
         return true
     }
 
---
+++ KidsFit/Pods/Target Support Files/Pods-KidsFit-KidsFitUITests/Pods-KidsFit-KidsFitUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KidsFit_KidsFitUITests : NSObject
+@end
+@implementation PodsDummy_Pods_KidsFit_KidsFitUITests
+@end
