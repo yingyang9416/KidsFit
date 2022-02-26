921d962f652698909631fbbbb6edf371958d8355
Ying Yang <yingyang9416@gmail.com>
2022-02-25 21:39:33 -0500

    removed pods folder to untrack pods

--- KidsFit/Pods/Target Support Files/Pods-KidsFit-KidsFitUITests/Pods-KidsFit-KidsFitUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KidsFit_KidsFitUITests : NSObject
-@end
-@implementation PodsDummy_Pods_KidsFit_KidsFitUITests
-@end
--- KidsFit/Pods/Target Support Files/Pods-KidsFitTests/Pods-KidsFitTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KidsFitTests : NSObject
-@end
-@implementation PodsDummy_Pods_KidsFitTests
-@end
